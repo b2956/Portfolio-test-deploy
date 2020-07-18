2d99c0d7daf54b9ea9f62b2f3d0ae224578c2428
Bruno Fávero <bruno.2956@gmail.com>
2020-07-17 22:02:15 -0300

    test deploy

---
+++ precache-manifest.99db83051b61b443e773c7e467d8c5b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e280ef18055a518d07d7a31410aad706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053375f0d8d53d80cbe2",
+    "url": "/static/css/main.325da6d4.chunk.css"
+  },
+  {
+    "revision": "e21c5841e0a95a9fc9b1",
+    "url": "/static/js/2.9784485a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9784485a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053375f0d8d53d80cbe2",
+    "url": "/static/js/main.d6bb7ed8.chunk.js"
+  },
+  {
+    "revision": "4dbed8ab34d4d5755ddd",
+    "url": "/static/js/runtime-main.906ebcfb.js"
+  },
+  {
+    "revision": "346c7878a21c0f07c763155ba8071892",
+    "url": "/static/media/computer-chair.346c7878.webp"
+  },
+  {
+    "revision": "58fccc55f55ada14b7bb7e642a21ec51",
+    "url": "/static/media/light-bulb.58fccc55.webp"
+  },
+  {
+    "revision": "22218750b5f4a7596385e7b0e11eb7cc",
+    "url": "/static/media/profile.22218750.jpg"
+  }
+]);
